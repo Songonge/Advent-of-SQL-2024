52b9cc60d6e6cb5f9739cc15bf4d09e1b6633a78
HleyHub <hashleyed@gmail.com>
2024-12-11 20:09:18 -0500

    The SQL file to solve day 5 question

---
+++ Day 5/day_5.sql
@@ -0,0 +1,23 @@
+WITH production_trends AS (
+  SELECT
+    production_date,
+    toys_produced,
+    LAG(toys_produced) OVER (ORDER BY production_date) AS previous_day_production,
+    toys_produced - LAG(toys_produced) OVER (ORDER BY production_date) AS production_change,
+    ROUND(
+      (toys_produced - LAG(toys_produced) OVER (ORDER BY production_date))::numeric /
+      NULLIF(LAG(toys_produced) OVER (ORDER BY production_date), 0) * 100,
+      2
+    ) AS production_change_percentage
+  FROM toy_production_new
+)
+SELECT production_date
+FROM production_trends
+WHERE production_change_percentage IS NOT NULL
+ORDER BY production_change_percentage DESC
+LIMIT 1
+;
+
+-- SELECT *
+-- FROM toy_production_new
+-- ;
