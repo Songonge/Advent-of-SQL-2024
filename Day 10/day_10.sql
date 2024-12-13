065afbf8f56f3866ef0fa90e0f7ab2358143eb4c
HleyHub <hashleyed@gmail.com>
2024-12-13 08:13:05 -0500

    The SQL file to solve day 10 question

--- Day 10/day_10.sql
+++ Day 10/day_10.sql
@@ -1,12 +1,30 @@
 SELECT
     date,
-    SUM(CASE WHEN drink_name = 'Eggnog' THEN quantity ELSE 0 END) AS eggnog,
-    SUM(CASE WHEN drink_name = 'Hot Cocoa' THEN quantity ELSE 0 END) AS hot_cocoa,
-    SUM(CASE WHEN drink_name = 'Peppermint Schnapps' THEN quantity ELSE 0 END) AS peppermint_schnapps
+    SUM(
+		CASE 
+			WHEN drink_name = 'Eggnog' THEN quantity 
+		ELSE 0 END) AS eggnog,
+    SUM(
+		CASE 
+			WHEN drink_name = 'Hot Cocoa' THEN quantity 
+		ELSE 0 END) AS hot_cocoa,
+    SUM(
+		CASE 
+			WHEN drink_name = 'Peppermint Schnapps' THEN quantity 
+		ELSE 0 END) AS peppermint_schnapps
 FROM Drinks
 GROUP BY date
 HAVING
-    SUM(CASE WHEN drink_name = 'Hot Cocoa' THEN quantity ELSE 0 END) = 38
-    AND SUM(CASE WHEN drink_name = 'Peppermint Schnapps' THEN quantity ELSE 0 END) = 298
-    AND SUM(CASE WHEN drink_name = 'Eggnog' THEN quantity ELSE 0 END) = 198
-ORDER BY date;
+	SUM(
+		CASE 
+			WHEN drink_name = 'Eggnog' THEN quantity 
+		ELSE 0 END) = 198
+    AND SUM(
+		CASE 
+			WHEN drink_name = 'Hot Cocoa' THEN quantity 
+		ELSE 0 END) = 38
+    AND SUM(
+		CASE 
+			WHEN drink_name = 'Peppermint Schnapps' THEN quantity 
+		ELSE 0 END) = 298
+;
