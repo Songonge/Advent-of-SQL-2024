1fd799c273b52acfa9cff58fed19908e14fef943
HleyHub <hashleyed@gmail.com>
2024-12-21 15:02:16 -0500

    The SQL file containing the solution to day 21 question

---
+++ Day 21/day_21.sql
@@ -0,0 +1,18 @@
+WITH total_amount AS (
+	SELECT
+		EXTRACT(YEAR FROM sale_date) yr,
+		EXTRACT(QUARTER FROM sale_date) quarter,
+		SUM(amount) AS total_sales,
+		LAG(SUM(amount), 1) OVER (ORDER BY EXTRACT(YEAR FROM sale_date), EXTRACT(QUARTER FROM sale_date)) AS previous_quarter_amount
+	FROM sales
+	GROUP BY yr, quarter
+)
+SELECT
+	yr,
+	quarter,
+	total_sales,
+	total_sales / previous_quarter_amount AS growth_rate
+FROM total_amount
+ORDER BY growth_rate DESC NULLS LAST
+LIMIT 1
+;
